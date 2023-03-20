aa63c9747ffb71932f27ef8713288602d910acd1
Staxxx <sersersin@gmail.com>
2023-03-20 21:12:29 +0300

    hot fix Wizard

--- HW/OOP_5/Wizard.java
+++ HW/OOP_5/Wizard.java
@@ -19,6 +19,33 @@ public class Wizard extends Creature {
 
     }
 
+    public void step(List<Creature> Team_A) {
+
+        Collections.sort(Team_A, new Comparator<Creature>() {
+            public int compare(Creature c1, Creature c2) {
+                if (c1.priorityHealerEnum.getCode() > c1.priorityHealerEnum.getCode()) {
+                    return 1;
+                } else {
+                    return -1;
+
+                }
+            }
+        });
+        for (Creature friend : Team_A) {
+
+            if (friend.getCurrentHealth() < friend.getMaxHealth() && friend.getCurrentHealth() != 0) {
+                friend.setCurrentHealth(getCurrentHealth() + 5);
+                if (friend.getCurrentHealth() > friend.getMaxHealth()) {
+                    friend.setCurrentHealth(friend.getMaxHealth());
+                }
+                return;
+            }
+
+        }
+    }
+
+    
+
     public String getInfo() {
         return "Я маг " + getName();
     }
